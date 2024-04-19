1e3be394f217331ff566b40af81359ef79c32875
Raikuro <jugragu22@gmail.com>
2024-04-19 20:12:19 +0200

    Fixed docker network

--- src/main/java/com/mca/config/ApiConfig.java
+++ src/main/java/com/mca/config/ApiConfig.java
@@ -1,16 +1,32 @@
 package com.mca.config;
 
+import com.mca.infrastructure.client.ApiClient;
 import com.mca.infrastructure.client.api.DefaultApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ApiConfig {
 
+    @Value("${saga.api.url}")
+    private String sagaApiUrl;
+
     @Bean
-    public DefaultApi gameSagaApi() {
-        return new DefaultApi();
+    public DefaultApi gameSagaApi(ApiClient apiClient) {
+        return new DefaultApi(apiClient.setBasePath(sagaApiUrl));
+    }
+
+    @Bean
+    public ApiClient apiClient(WebClient webClient){
+        return new ApiClient(webClient);
+    }
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder().build();
     }
 
     @Bean
--- src/main/java/com/mca/service/SagaService.java
+++ src/main/java/com/mca/service/SagaService.java
@@ -1,15 +1,16 @@
 package com.mca.service;
 
 import com.mca.exception.GameNotFoundException;
+import com.mca.infrastructure.client.ApiClient;
 import com.mca.infrastructure.client.api.DefaultApi;
 import com.mca.model.Game;
 import com.mca.repository.GameRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
-import java.sql.Timestamp;
-import java.time.Instant;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -17,6 +18,7 @@ import java.util.stream.Collectors;
 import static java.sql.Timestamp.from;
 import static java.time.Instant.parse;
 
+@Slf4j
 @Service
 public class SagaService {
 
@@ -29,14 +31,29 @@ public class SagaService {
     @Autowired
     private DefaultApi gameSagaApi;
 
+    @Value("${saga.api.url}")
+    private String sagaApiUrl;
+
     public Set<Game> getSaga(String gameId) {
+
+        ApiClient apiClient = gameSagaApi.getApiClient();
+        apiClient.setBasePath(sagaApiUrl);
+        gameSagaApi.setApiClient(apiClient);
+        LinkedHashSet<String> asd = gameSagaApi.getGameSagaRelatedSagas(gameId).block();
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$ ->" + asd);
+
         Set<Integer> videogamesIdsInSaga = gameSagaApi
                 .getGameSagaRelatedSagas(gameId)
-                .onErrorMap(x -> new GameNotFoundException(gameId))
+                .onErrorMap(x -> {
+                    log.warn(((WebClientResponseException) x).getStatusCode() + "");
+                    return new GameNotFoundException(gameId);
+                })
                 .block()
                 .stream()
                 .map(Integer::valueOf)
                 .collect(Collectors.toCollection(LinkedHashSet::new));
+
+
 
         Map<Integer, Game> gameMap = gameRepository.findGameInfoByIdsAndDate(videogamesIdsInSaga, from(parse(date)))
                 .stream()
