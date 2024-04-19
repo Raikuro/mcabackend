384a638f466184907c48d127287d786d5b961e9c
Raikuro <jugragu22@gmail.com>
2024-04-19 20:21:07 +0200

    Minor fixes

--- src/main/java/com/mca/service/SagaService.java
+++ src/main/java/com/mca/service/SagaService.java
@@ -1,7 +1,6 @@
 package com.mca.service;
 
 import com.mca.exception.GameNotFoundException;
-import com.mca.infrastructure.client.ApiClient;
 import com.mca.infrastructure.client.api.DefaultApi;
 import com.mca.model.Game;
 import com.mca.repository.GameRepository;
@@ -9,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.*;
 import java.util.function.Function;
@@ -36,16 +34,10 @@ public class SagaService {
 
     public Set<Game> getSaga(String gameId) {
 
-        ApiClient apiClient = gameSagaApi.getApiClient();
-        apiClient.setBasePath(sagaApiUrl);
-        gameSagaApi.setApiClient(apiClient);
-        LinkedHashSet<String> asd = gameSagaApi.getGameSagaRelatedSagas(gameId).block();
-        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$ ->" + asd);
-
         Set<Integer> videogamesIdsInSaga = gameSagaApi
                 .getGameSagaRelatedSagas(gameId)
                 .onErrorMap(x -> {
-                    log.warn(((WebClientResponseException) x).getStatusCode() + "");
+                    log.warn(x.toString());
                     return new GameNotFoundException(gameId);
                 })
                 .block()
